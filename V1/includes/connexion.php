8a77aedd9a111f0a657679747e2b07592e7d5733
Alixx <aliahsakina2@gmail.com>
2025-07-14 11:48:04 +0300

    connex

--- V1/includes/connexion.php
+++ V1/includes/connexion.php
@@ -1,5 +1,5 @@
 <?php 
-    if($bdd = mysqli_connect('localhost','root','','emprunt_objets'))
+    if($bdd = mysqli_connect('localhost','ETU004352','eGKElguo','db_s2_ETU004352'))
     {
         echo '';
     }
